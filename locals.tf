74df9d0941cf452f01064eb19974db83d8e8884a
SharmilaD <sharmiladaram419@gmail.com>
2025-06-25 16:19:25 +0530

    updating repo

--- locals.tf
+++ locals.tf
@@ -4,12 +4,4 @@ locals {
     Environment = var.environment
     Terraform = "true"
   }
-
-  tags = merge(
-    var.sg_tags,
-    local.common_tags,
-    {
-      "Name = ${var.proejct}-${var.environment}-${var.sg_name}"
-    }
-  )
 }
